f8729bb3a33fcfd696aafd93edfead790a70e3c4
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 15:52:44 -0500

    #9 1. add toast in Versions

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -9,5 +9,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
+
+        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
     }
 }

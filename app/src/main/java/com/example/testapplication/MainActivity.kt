7f6548504ef9c6d8e4aa5eba129e7b4c0407d2dc
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 15:23:16 -0500

    #4 1.Add  a Toast

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< Updated upstream
+=======
+        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
+>>>>>>> Stashed changes
     }
 }

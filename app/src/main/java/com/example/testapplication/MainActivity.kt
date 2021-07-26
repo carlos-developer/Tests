4819f5144089d560c7cef8d70d0e25a8d78101b8
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 22:08:01 -0500

    #42 1.Cambios en main

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -65,9 +65,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun nothingmain5() {
-        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main_2", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main_2", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main_2", Toast.LENGTH_LONG).show()
 
     fun nothingmain3() {
         Toast.makeText(this, "Version_17", Toast.LENGTH_LONG).show()

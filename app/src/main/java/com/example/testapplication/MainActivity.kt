b95743788ab6851056c77991e6280688b2f53b24
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 20:39:28 -0500

    #36 cambios en main para probar rama 15

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -11,15 +11,13 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Version_0.12", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
 
-        Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version main", Toast.LENGTH_LONG).show()
 
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.4", Toast.LENGTH_LONG).show()
 
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
     }
@@ -37,6 +35,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun nothingmain() {
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+    }
+
+    fun nothingmain2() {
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()

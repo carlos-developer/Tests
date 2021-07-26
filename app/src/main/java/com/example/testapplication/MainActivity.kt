455e429d4e77b3c64b9a110b6d1ca2e9862d62c1
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 20:12:40 -0500

    #33 Cambios en la rama main

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -14,13 +14,21 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
 
         Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version main", Toast.LENGTH_LONG).show()
 
-        Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.4", Toast.LENGTH_LONG).show()
 
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+    }
+
+    fun nothing() {
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+    }
+
+    fun nothing2() {
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
     }
 }

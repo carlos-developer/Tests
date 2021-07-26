5cf79fdd632e899f9d23358d9e63c0cec8ee7a68
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 19:16:22 -0500

    #25 Commit que se va a merge con main

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -8,10 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Version_0.6", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.6", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.6", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_0.7", Toast.LENGTH_LONG).show()
 
         Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()

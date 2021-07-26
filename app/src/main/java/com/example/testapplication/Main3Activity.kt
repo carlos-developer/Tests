1c922706849fa30dbd25c498067f4e7480640e38
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 22:26:32 -0500

    #43 Cambios en main

---
+++ app/src/main/java/com/example/testapplication/Main3Activity.kt
@@ -0,0 +1,24 @@
+package com.example.testapplication
+
+import android.widget.Toast
+
+class Main3Activity {
+
+    fun nothingmain2() {
+        Toast.makeText(this, "Version_main_1", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main_2", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main_3", Toast.LENGTH_LONG).show()
+    }
+
+    fun nothingmain3() {
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+    }
+
+    fun nothingmain4() {
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+    }
+}

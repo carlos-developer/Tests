8f0d5eb4527355bc82cb91787f7bc7c0a438a866
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 19:51:54 -0500

    #31 Adiciona cambios en main

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -20,5 +20,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.4", Toast.LENGTH_LONG).show()
+
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
     }
 }

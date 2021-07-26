d00531f252959c46c5778e665cdb56c1b6f1eb7e
Juan Carlos Suarez Marin <juan.suarez@gopenux.com>
2021-07-25 20:23:41 -0500

    #35 cambios en main

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -18,19 +18,25 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Version main", Toast.LENGTH_LONG).show()
 
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_0.5", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_0.4", Toast.LENGTH_LONG).show()
 
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
     }
 
     fun nothing() {
-        Toast.makeText(this, "Version_main_2", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Version_main_2", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
     }
 
     fun nothing2() {
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
+    }
+
+    fun nothingmain() {
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Version_main", Toast.LENGTH_LONG).show()
